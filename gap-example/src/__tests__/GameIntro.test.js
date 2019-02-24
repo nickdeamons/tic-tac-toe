96e3609226130adaf3c90cbbbdb82dde7f4ee6d8
nddynamit <nick.deamons@dynamit.com>
2019-02-24 08:19:23 -0500

    Removed extra node_modules folder

---
+++ gap-example/src/__tests__/GameIntro.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {mount} from 'enzyme';
+//import GameStatus from '../components/Game/GameStatus';
+//import Player from '../models/Player';
+
+describe('Game splash screen, intro', () => {
+  it('renders as the default route', () => {
+    
+  })
+})
